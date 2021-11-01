1b79fbbdc6c6398655721b641db32040176ea634
JuliusPanduro <j.panduro.r@hotmail.com>
2021-11-01 21:30:29 +0100

    Front-end for Galleries v.0.0.1.1

---
+++ 9._Galleries_Frontend/Header/Header.js
@@ -0,0 +1,13 @@
+document.writeln("<header id=\"header\">\n" +
+    "    <p class=\"logo\">\n" +
+    "        <img src=\"../lib/d4hxksf-f93d73e4-6e76-477b-a675-526326b4576f.png\" width=\"100\" height=\"100\" alt=\"\">\n" +
+    "    </p>\n" +
+    "\n" +
+    "    <ul class=\"menuUl\">\n" +
+    "        <li class=\"active\"><a href=\"../Frontpage/Frontpage.html\">Home</a></li>\n" +
+    "        <li><a class=\"a_Artists\" href=\"../Artists/Artists.html\">Artists</a></li>\n" +
+    "        <li><a class=\"a_Galleries\" href=\"../Galleries/Galleries.html\">Galleries</a></li>\n" +
+    "        <li><a class=\"a_Paintings\" href=\"../Paintings/Paintings.html\">Paintings</a></li>\n" +
+    "    </ul>\n" +
+    "</header>")
+
