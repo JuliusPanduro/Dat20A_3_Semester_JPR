75bbfba7a8d2bbabccb5fcf3be185c22db7f43d7
JuliusPanduro <juli05d6@stud.kea.dk>
2021-11-02 09:22:47 +0100

    Nav Innerhtml added

--- 9._Galleries_Frontend/Header/Header.js
+++ 9._Galleries_Frontend/Header/Header.js
@@ -1,4 +1,15 @@
-document.writeln("<header id=\"header\">\n" +
+document.getElementById("myNav").innerHTML =
+    " <a class=\"logo\" href=\"../Frontpage/Frontpage.html\">\n" +
+    "            <img id=\"logo\" src=\"../lib/d4hxksf-f93d73e4-6e76-477b-a675-526326b4576f.png\" width=\"100\" height=\"100\" alt=\"Logo Art\">\n" +
+    "        </a>\n" +
+    "        <ul class=\"menuUl\">\n" +
+    "            <li><a class=\"a_Artists\" href=\"../Artists/Artists.html\">Artists</a></li>\n" +
+    "            <li><a class=\"a_Galleries\" href=\"../Galleries/Galleries.html\">Galleries</a></li>\n" +
+    "            <li><a class=\"a_Paintings\" href=\"../Paintings/Paintings.html\">Paintings</a></li>\n" +
+    "        </ul>";
+
+
+/*document.writeln("<header id=\"header\">\n" +
     "    <nav>\n" +
     "        <a class=\"logo\" href=\"../Frontpage/Frontpage.html\">\n" +
     "            <img id=\"logo\" src=\"../lib/d4hxksf-f93d73e4-6e76-477b-a675-526326b4576f.png\" width=\"100\" height=\"100\" alt=\"Logo Art\">\n" +
@@ -13,3 +24,4 @@ document.writeln("<header id=\"header\">\n" +
     "\n" +
     "</header>");
 
+ */
